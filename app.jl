71b7da0430c5633a571b4e9b82fa3196193b621f
Josh Day <emailjoshday@gmail.com>
2019-03-11 14:56:03 -0400

    wip

--- app.jl
+++ app.jl
@@ -3,11 +3,8 @@ using Mux
 @app test = (
   Mux.defaults,
   page(respond("<h1>Hello World!!</h1>")),
-  page("/about",
-       probabilty(0.1, respond("<h1>Boo!</h1>")),
-       respond("<h1>About Me</h1>")),
-  page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
+  page("/about", respond("<h1>About Me</h1>")),
   Mux.notfound())
 
 @info "Serving app on $(ARGS[1])"
-fetch(serve(test, ARGS[1]))
+fetch(serve(test, parse(Int64, ARGS[1])))
