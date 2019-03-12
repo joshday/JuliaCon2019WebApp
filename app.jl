43c11f8fa76e6c10359f06ef3fa37d705d8dfcda
Josh Day <emailjoshday@gmail.com>
2019-03-12 05:59:40 -0400

    wip

--- app.jl
+++ app.jl
@@ -1,12 +1,16 @@
-using Mux, Interact, CSSUtil, WebIO, Plots
-plotly()
+using Observables, Interact, CSSUtil, WebIO
 
 const PORT = parse(Int64, ARGS[1])
 
 #-----------------------------------------------------------------------# ui
-ui = @manipulate for i in 1:100
-    plot(rand(i))
-end
+x1 = slider(1:50)
+x1out = Observable("")
+on(x -> x1out[] = "slider's values is $x", observe(x1))
+
+ui = vbox(
+    x1, x1out, CSSUtil.hline(),
+    "Hi"
+)
 
 #-----------------------------------------------------------------------# Run Server
 WebIO.webio_serve(page("/", req -> ui), PORT)
