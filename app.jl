ccefb41258b5fab9ec347a25f3098b424397e96c
Josh Day <emailjoshday@gmail.com>
2019-03-19 17:18:30 -0400

    wip

--- app.jl
+++ app.jl
@@ -1,15 +1,31 @@
-using Mux, Observables, Interact, CSSUtil, WebIO
+using Mux, Observables, Interact, CSSUtil, WebIO, Markdown
 
 const PORT = parse(Int64, ARGS[1])
 
 #-----------------------------------------------------------------------# ui
 x1 = slider(1:50) |> onchange
-x1out = Observable("")
-on(x -> x1out[] = "slider's values is $x", observe(x1))
+x1out = @map "The slider's value is $(&x1)"
+
+
+x2 = checkboxes(1:5)
+x2out = @map "You have checked: $(&x2)"
 
 ui = vbox(
-    x1, x1out, CSSUtil.hline(),
-    "Hi"
+    md"""
+    # Welcome to a tour of [Interact.jl](https://github.com/JuliaGizmos/Interact.jl)!
+    """,
+
+    md"""
+    ## Sliders
+    """
+    x1, x1out,
+
+    md"""
+    ## Checkboxes
+    """,
+    x2, x2out,
+
+
 )
 
 #-----------------------------------------------------------------------# Run Server
