8a5c68174440fba2a76ddf5004a08a69a4a5ef30
Josh Day <emailjoshday@gmail.com>
2019-03-19 17:11:11 -0400

    wip

--- app.jl
+++ app.jl
@@ -3,7 +3,7 @@ using Mux, Observables, Interact, CSSUtil, WebIO
 const PORT = parse(Int64, ARGS[1])
 
 #-----------------------------------------------------------------------# ui
-x1 = slider(1:50)
+x1 = slider(1:50) |> onchange
 x1out = Observable("")
 on(x -> x1out[] = "slider's values is $x", observe(x1))
 
