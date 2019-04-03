3a402201656dfae2b32416f753c6bf3be2c61770
Josh Day <emailjoshday@gmail.com>
2019-04-03 16:03:59 -0400

    wip

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: julia --project=$(pwd) app.jl $PORT
+web: julia --project=@. app.jl $PORT
--- app.jl
+++ app.jl
@@ -1,17 +1,19 @@
 using Mux, Observables, Interact, CSSUtil, WebIO, Markdown, Dates
 using Observables: @map
 
-const PORT = parse(Int64, ARGS[1])
+const PORT = isempty(ARGS) ? 8888 : parse(Int64, ARGS[1])
 
 #-----------------------------------------------------------------------# ui
+@info "Defining UI..."
+
 x1 = slider(1:50) |> onchange
 x1out = @map "The slider's value is $(&x1)"
 
 x2 = checkboxes(1:5)
 x2out = @map "You have checked: $(&x2)"
 
-x3 = datepicker(today())
-x3out = @map "Date picker widget is $(&x3)"
+x3 = widget([:on, :off])
+x3out = @map "On or off? $(&x3)"
 
 x4 = textbox("Enter text here")
 x4out = @map "Echo: $(&x4)"
@@ -26,7 +28,8 @@ ui = vbox(
     x3, x3out,
     x4, x4out,
     x5, x5out
-)
+)(style("padding" => "5px"))
 
 #-----------------------------------------------------------------------# Run Server
-WebIO.webio_serve(page("/", req -> ui), PORT)
+@info "Running server..."
+fetch(WebIO.webio_serve(page("/", req -> ui), PORT))
